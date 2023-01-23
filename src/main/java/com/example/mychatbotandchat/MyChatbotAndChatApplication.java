46763b20f8953df6512d70c00107141bfb4682d4
엄준영 <eomjunyoung@eomjun-yeong-ui-MacBookPro.local>
2023-01-24 07:29:13 +0900

    Initial commit

---
+++ src/main/java/com/example/mychatbotandchat/MyChatbotAndChatApplication.java
@@ -0,0 +1,14 @@
+package com.example.mychatbotandchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChatbotAndChatApplication {
+
+    public static void main(String[] args) {
+        System.out.println("testss");
+        SpringApplication.run(MyChatbotAndChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mychatbotandchat/MyChatbotAndChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mychatbotandchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatbotAndChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
